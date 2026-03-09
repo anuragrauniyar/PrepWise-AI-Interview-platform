1aec3964740285537558bfcce026f8e283aac9f0
Anurag <anurag1822003@gmail.com>
2026-03-09 17:36:28 +0530

    updated admin.ts

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,24 +1,24 @@
-import {initializeApp, getApps, cert} from 'firebase-admin/app';
-import {getAuth} from "firebase-admin/auth";
-import {getFirestore} from "firebase-admin/firestore";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
-const initFirebaseAdmin = () => {
-    const apps = getApps();
+let db;
 
-    if(!apps.length) {
-        initializeApp({
-            credential: cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY?.split(String.raw`\n`).join('\n')
-            })
-        })
-    }
+if (!getApps().length) {
+  const app = initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
 
-    return {
-        auth: getAuth(),
-        db: getFirestore(),
-    }
+  db = getFirestore(app);
+  db.settings({ ignoreUndefinedProperties: true });
+
+} else {
+  db = getFirestore();
 }
 
-export const { auth, db } = initFirebaseAdmin();
+export const auth = getAuth();
+export { db };
